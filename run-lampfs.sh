ad61eca099319e3a15f01143f1edfd8fb744f159
Michael Prinsloo <mike.prinsloo@gmail.com>
2015-09-26 15:10:08 +0200

    Added phpini volume to lampfs.

--- run-lampfs.sh
+++ run-lampfs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker run --name lampfs -v /var/lampdev/www:/var/www/html:rw -v /var/lampdev/mysql:/var/lib/mysql:rw -v /var/lampdev/logs:/var/log/apache2:rw gophernoob/lampfs
+sudo docker run --name lampfs -v /var/lampdev/phpini:/etc/php5/apache2:rw -v /var/lampdev/www:/var/www/html:rw -v /var/lampdev/mysql:/var/lib/mysql:rw -v /var/lampdev/logs:/var/log/apache2:rw gophernoob/lampfs
